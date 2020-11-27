43a3ca627f525627df7c06f18434db2ea955527f
rai-watanabe <55418247+rai-wtnb@users.noreply.github.com>
2020-11-27 19:13:23 +0900

    feat: created file upload function (#20)

--- api/go.mod
+++ api/go.mod
@@ -3,11 +3,11 @@ module github.com/rai-wtnb/accomplist-api
 go 1.15
 
 require (
-	github.com/aws/aws-sdk-go v1.35.35 // indirect
+	github.com/aws/aws-sdk-go v1.35.35
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/gorm v1.9.16
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/joho/godotenv v1.3.0
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
 )
 
@@ -19,4 +19,5 @@ replace (
 	github.com/rai-wtnb/accomplist-api/server => ./server
 	github.com/rai-wtnb/accomplist-api/utils/crypto => ./utils/crypto
 	github.com/rai-wtnb/accomplist-api/utils/mysession => ./utils/mysession
+	github.com/rai-wtnb/accomplist-api/utils/s3 => ./utils/s3
 )
--- api/utils/s3/s3.go
+++ api/utils/s3/s3.go
@@ -3,22 +3,54 @@ package s3
 import (
 	"os"
 	"log"
+	"fmt"
+	"errors"
+	"bytes"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
-func Upload(file string) {
+func Upload(file []byte) (string, error) {
 	loadEnv()
-	sess := session.Must(session.NewSession(&aws.Config{
-		Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS"), os.GetEnv("AWS_SECRET"), ""),
-	  Region: aws.String("ap-northeast-1"),
-	 }))
+
+	//todo
+	fileUrl, err := sendToS3(file, "empty.png")
+	if err != nil {
+		return fileUrl, err
+	}
+
+	//fileUrlをデータベースに記録する
+	return fileUrl, nil
 }
 
-func Get() {
-	log.Println("get from s3")
+func sendToS3(file []byte, fileName string) (string, error) {
+	contentType := getContentType(filepath.Ext(fileName))
+	if contentType == "" {
+		return "", errors.New("Unknown content type")
+	}
+
+	uploader := uploader()
+
+	key := fmt.Sprintf("uploads/%s", fileName)
+
+	_, err := uploader.Upload(&s3manager.UploadInput{
+		Body:        bytes.NewReader(file),
+		Bucket:      aws.String(os.Getenv("AWS_BUCKET")),
+		ContentType: aws.String(contentType),
+		Key:         aws.String(key),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	fileURL := fmt.Sprintf("https://s3-%s.amazonaws.com/%s/%s", os.Getenv("AWS_REGION"), os.Getenv("AWS_BUCKET"), key)
+
+	return fileURL, nil
 }
 
 // loadEnv prepare to use env.
@@ -27,4 +59,33 @@ func loadEnv() {
     if err != nil {
         log.Fatal("Error: failed to load .env file")
     }
+}
+
+func uploader() *s3manager.Uploader {
+	return s3manager.NewUploader(session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Credentials: credentials.NewStaticCredentialsFromCreds(credentials.Value{
+				AccessKeyID:     os.Getenv("AWS_ACCESS"),
+				SecretAccessKey: os.Getenv("AWS_SECRET"),
+			}),
+			Region: aws.String(os.Getenv("AWS_REGION")),
+		},
+	})))
+}
+
+
+// getContentType judges file file type.
+func getContentType(extension string) string {
+	switch extension {
+	case "jpg":
+		return "image/jpeg"
+	case "jpeg":
+		return "image/jpeg"
+	case "gif":
+		return "image/gif"
+	case "png":
+		return "image/png"
+	default:
+		return ""
+	}
 }
