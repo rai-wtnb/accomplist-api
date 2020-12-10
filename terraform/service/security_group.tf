a2121bf29c1d7f4a0afd02dbeb460281dbc0fe9b
rai-watanabe <55418247+rai-wtnb@users.noreply.github.com>
2020-12-10 14:53:47 +0900

    feat: created alb (#9)

--- terraform/service/security_group.tf
+++ terraform/service/security_group.tf
@@ -3,6 +3,38 @@ resource "aws_security_group" "instance" {
   description = "instance sg"
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    security_groups = [
+      aws_security_group.alb.id,
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name        = "accomplist-alb"
+  description = "http and https"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "TCP"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
