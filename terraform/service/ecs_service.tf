736ddfc9f574ec246b3679c594b178cac5d44ca6
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2020-12-10 14:52:06 +0900

    feat: created alb

--- terraform/service/ecs_service.tf
+++ terraform/service/ecs_service.tf
@@ -4,4 +4,9 @@ resource "aws_ecs_service" "accomplist-api" {
   task_definition = aws_ecs_task_definition.accomplist-task.arn
   desired_count   = 1
   launch_type     = "EC2"
+  load_balancer {
+    target_group_arn = aws_lb_target_group.http.arn
+    container_name   = "accomplist-api"
+    container_port   = "4567"
+  }
 }
--- terraform/vpc/subnet.tf
+++ terraform/vpc/subnet.tf
@@ -2,4 +2,16 @@ resource "aws_subnet" "public_subnet_1" {
   vpc_id            = "${aws_vpc.vpc.id}"
   cidr_block        = "10.0.0.0/20"
   availability_zone = "ap-northeast-1a"
+  tags = {
+    "Name" = "accomplist-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "10.0.16.0/20"
+  availability_zone = "ap-northeast-1c"
+  tags = {
+    "Name" = "accomplist-subnet-2"
+  }
 }
