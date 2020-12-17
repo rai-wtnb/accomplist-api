858f7cfa12059fa5ca89753b37da6fa1fa4642c6
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2020-12-17 12:07:15 +0900

    fix: modified network

--- terraform/service/lb_target_group.tf
+++ terraform/service/lb_target_group.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "http" {
   name     = "accomplist-http"
-  port     = 4567
+  port     = 80
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.vpc_id
   health_check {
