6ec95c356cdc8d5489e4c43b0397fd5db58bef56
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2020-12-27 08:19:30 +0900

    fix: modified s3 upload error

--- terraform/service/security_group.tf
+++ terraform/service/security_group.tf
@@ -13,16 +13,16 @@ resource "aws_security_group" "instance" {
   }
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -38,15 +38,6 @@ resource "aws_security_group" "alb" {
   name        = "accomplist-alb"
   description = "http and https"
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
-
-  ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "TCP"
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
 
   ingress {
     from_port = 443
