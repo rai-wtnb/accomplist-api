c972422a9cc214d7cdd848a612faebf4eb9ff3d7
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2021-01-16 15:05:45 +0900

    fix: modified list and feedback controller

--- api/controllers/list.go
+++ api/controllers/list.go
@@ -1,9 +1,10 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rai-wtnb/accomplist-api/models/repository"
-	"net/http"
 )
 
 type ListController struct{}
@@ -11,76 +12,80 @@ type ListController struct{}
 // Index : GET /lists
 func (ListController) Index(c *gin.Context) {
 	var l repository.ListRepository
+
 	r, err := l.GetAll()
 	if err != nil {
-		c.AbortWithStatus(404)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	} else {
-		c.JSON(200, r)
+		return
 	}
+
+	c.JSON(200, r)
 }
 
 // Create : POST /lists/
 func (ListController) Create(c *gin.Context) {
 	var l repository.ListRepository
+
 	r, err := l.CreateList(c)
 	if err != nil {
-		c.AbortWithStatus(400)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	} else {
-		c.JSON(201, r)
+		return
 	}
+
+	c.JSON(201, r)
 }
 
-// Show : GET /lists/specific/:id
+// IndexByUserID : GET /lists/specific/:id
 func (ListController) IndexByUserID(c *gin.Context) {
-	id := c.Params.ByName("id")
 	var l repository.ListRepository
+	id := c.Params.ByName("id")
+
 	list, err := l.GetByUserID(id)
 	if err != nil {
-		c.AbortWithStatus(400)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	} else {
-		c.JSON(200, list)
+		return
 	}
+
+	c.JSON(200, list)
 }
 
 // Show : GET /lists/specific/:id
 func (ListController) Show(c *gin.Context) {
-	id := c.Params.ByName("id")
 	var l repository.ListRepository
+	id := c.Params.ByName("id")
+
 	r, err := l.GetByListID(id, c)
 	if err != nil {
-		c.AbortWithStatus(404)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	} else {
-		c.JSON(200, r)
+		return
 	}
+
+	c.JSON(200, r)
 }
 
 // Update : PUT /lists/specific/:id
 func (ListController) Update(c *gin.Context) {
-	id := c.Params.ByName("id")
 	var l repository.ListRepository
+	id := c.Params.ByName("id")
+
 	r, err := l.UpdateByID(id, c)
 	if err != nil {
-		c.AbortWithStatus(404)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	} else {
-		c.JSON(200, r)
+		return
 	}
+
+	c.JSON(200, r)
 }
 
 // Delete : DELETE /lists/specific/:id
 func (ListController) Delete(c *gin.Context) {
 	id := c.Params.ByName("id")
 	var l repository.ListRepository
-	err := l.DeleteByID(id)
-	if err != nil {
-		c.AbortWithStatus(403)
+
+	if err := l.DeleteByID(id); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(200, gin.H{"success": "リストを削除しました"})
-	return
+
+	c.JSON(200, gin.H{"success": "deleted the list"})
 }
